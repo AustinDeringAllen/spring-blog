871223e22a255d04366be1a7b3edf98c9f815c33
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-10-09 09:52:56 -0500

    finished rest of spring views

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,21 +1,31 @@
 package com.codeup.blog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @RequestMapping(path="/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String postIndex() {
-        return "posts index page";
+    public String postIndex(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        for(int i=0; i<2; i++) {
+            Post post = new Post("Post " + i, "This is post " + i);
+            posts.add(post);
+        }
+
+        model.addAttribute("posts",posts);
+        return "/posts/index";
     }
 
     @RequestMapping(path="/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String individualPost(@PathVariable long id) {
-        return "view an individual post";
+    public String individualPost(@PathVariable long id, Model model) {
+        Post post = new Post("I miss you", "I miss the good old days");
+        model.addAttribute("post",post);
+        return "/posts/show";
     }
 
     @RequestMapping(path="/posts/create", method = RequestMethod.GET)
